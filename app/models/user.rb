cc9db35ceaa893f977c5d36dbb92e18359bb9a03
Chris Jordan <christopher.james.jordan@gmail.com>
2013-11-20 14:03:59 -0700

    adding user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
-	has_many :review, :proposal
-	devise :database_authoenticatable, :trackable, :timeouttable, :locktable
-	attr_accessible :first_name, :last_name, :major, :email, :password, :password_confirmation
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 IsItFeasible::Application.routes.draw do
+  devise_for :users
+
   #map '/'' to '/login'
   root :to => "home#index"
 end
