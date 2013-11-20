c60206c83a0cb695a523c8356a5df2783e5ed95e
Chris Jordan <christopher.james.jordan@gmail.com>
2013-11-20 14:37:33 -0700

    with routes and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 IsItFeasible::Application.routes.draw do
-  devise_for :users, :path => "auth", :path_names => { :sign_in => 'login', :sign_out => 'logout',:password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
+  devise_for :users, :path => "auth", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
 
   #map '/'' to '/login'
   root :to => "home#login"
