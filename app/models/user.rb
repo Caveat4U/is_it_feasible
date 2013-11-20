9d3671bf22a9d0d3234e966f1b5de999a25c2b82
Chris Jordan <christopher.james.jordan@gmail.com>
2013-11-20 13:38:32 -0700

    removed rdoc log

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
 	has_many :review, :proposal
+	devise :database_authoenticatable, :trackable, :timeouttable, :locktable
+	attr_accessible :first_name, :last_name, :major, :email, :password, :password_confirmation
 end
