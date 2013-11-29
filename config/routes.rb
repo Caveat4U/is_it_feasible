089cf03d06aea17897108856d29bf491b4eba369
Chris Jordan <christopher.james.jordan@gmail.com>
2013-11-29 13:28:41 -0700

    current and cleaning up views from devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 IsItFeasible::Application.routes.draw do
-  resources :users, :proposals, :reviews
+	devise_for :users, :path => "auth", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
 
-  devise_for :users, :path => "auth", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
+	root :to => 'user#dashboard'
 
-  root :to => 'user#dashboard'
-
-  namespace :user do
-  	root :to => 'user#dashboard'
-  end
+	namespace :user do
+		root :to => 'user#dashboard'
+		devise_for :users, :path => "auth", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
+	end
 end
