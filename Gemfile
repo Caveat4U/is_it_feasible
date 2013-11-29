d66901225333fbf607c217702ad0883acd679902
Chris Jordan <christopher.james.jordan@gmail.com>
2013-11-29 12:53:16 -0700

    basic framework in place with bootstrap

--- Gemfile
+++ Gemfile
@@ -1,8 +1,11 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.1.0'
-
 gem 'devise', " ~> 2.2.8"
+gem "therubyracer"
+gem "less-rails" #Sprockets (what Rails 3.1 uses for its asset pipeline) supports LESS
+gem "twitter-bootstrap-rails"
+
 # Bundle edge Rails instead:
 # gem 'rails',     :git => 'git://github.com/rails/rails.git'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 IsItFeasible::Application.routes.draw do
+  resources :users, :proposals, :reviews
+
   devise_for :users, :path => "auth", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
 
-  root to: redirect("/auth/login")
+  root :to => 'user#dashboard'
 
   namespace :user do
-  	root :to redirect("/user")
+  	root :to => 'user#dashboard'
   end
-
-  match 'dashboard' => 'user_dashboard#index', :as => 'user_root'
 end
