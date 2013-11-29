93d0bcab31408c58b9edd964a202ba48f22483e4
Chris Jordan <christopher.james.jordan@gmail.com>
2013-11-29 16:53:55 -0700

    updating new user session

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,2 @@
 class UserController < ApplicationController
-	#def new_session
-	#	render template: "devise/sessions/new", layout: "layouts/user"
-	#end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,12 @@ IsItFeasible::Application.routes.draw do
 
   resources :proposals
 
-	devise_for :users, :path => "auth", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
+	devise_for :users, :path => "auth", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :registration => 'register', :sign_up => 'cmon_let_me_in' }
 
 	root :to => 'user#dashboard'
 
 	namespace :user do
 		resources :proposals, :reviews
 		root :to => 'user#dashboard'
-		get "sign_in", :to => "devise/sessions#new"
 	end
 end
