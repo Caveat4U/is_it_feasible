274e5008b0ae419421699573dc4a86a8f2dc7e59
Chris Jordan <christopher.james.jordan@gmail.com>
2013-11-29 14:38:53 -0700

    resolving merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 IsItFeasible::Application.routes.draw do
   resources :proposals
 
-	devise_for :users, :path => "auth", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
+	devise_for :users, :path => "auth", :path_names => { :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
 
 	root :to => 'user#dashboard'
 
