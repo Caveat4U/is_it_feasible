4766e1b9a733ae84d08334fa52687d188190adad
Chris Jordan <christopher.james.jordan@gmail.com>
2013-12-06 17:45:11 -0700

    fixed issues

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 # Class review exists to store the appropriate data for reviews of proposals
 class Review < ActiveRecord::Base
-	# A review belongs to a single user and proposal pair
-	belongs_to :user, :proposal
+	# A review belongs to a single user
+	belongs_to :user
 
 	# Setup accessible (or protected) attributes for model user
 	attr_accessible :proposal_id, :user_id, :comment, :rating, :plausible_language, :user_interested, :created_at, :updated_at
