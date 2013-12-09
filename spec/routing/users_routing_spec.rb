0b35c2640b5a5214c607da9fd3fcb09835d905ca
Chris Sterling <sterlincexc@colorado.edu>
2013-12-09 15:57:26 -0800

    72 examples, 49 failures, 6 pending

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -3,32 +3,28 @@ require "spec_helper"
 describe UserController do
   describe "routing" do
 
-    it "routes to #index" do
-      get("/users").should route_to("users#index")
+    it "routes to registration page" do
+      get("/auth/register/cmon_let_me_in").should route_to("devise/registrations#new")
     end
 
-    it "routes to #new" do
-      get("/users/new").should route_to("users#new")
-    end
-
-    it "routes to #show" do
-      get("/users/1").should route_to("users#show", :id => "1")
-    end
+#    it "routes to #show" do
+#      get("/users/1").should route_to("users#show", :id => "1")
+#    end
 
     it "routes to #edit" do
-      get("/users/1/edit").should route_to("users#edit", :id => "1")
-    end
-
-    it "routes to #create" do
-      post("/users").should route_to("users#create")
+      get("/auth/register/edit").should route_to("devise/registrations#edit", :id => "1")
     end
 
     it "routes to #update" do
-      put("/users/1").should route_to("users#update", :id => "1")
+      post("/auth/register").should route_to("devise/registrations#update")
     end
 
+#    it "routes to #update" do
+#      put("/users/1").should route_to("users#update", :id => "1")
+#    end
+
     it "routes to #destroy" do
-      delete("/users/1").should route_to("users#destroy", :id => "1")
+      delete("/auth/register").should route_to("device/registrations#destroy")
     end
 
   end
