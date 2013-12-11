791067c8bdf8ba361927261fa86a2f463d3c7ec9
Chris Sterling <sterlincexc@colorado.edu>
2013-12-11 12:03:20 -0800

    Adding user seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#User.create({ email: 'e@gmail.com'}, {encrypted_password: '$2a$10$vHCJorsbez7ZqC/SlHp5Vu4fJ8lL/cQf2d9vh4Ub0o9FiZpI.l.aG'} )
+
+#User.new({ :email => 'b@gmail.com'}, {:first_name => 'First'}, {:last_name => 'Last'}, {:major => 'Business'}, {:encrypted_password => '$2a$10$vHCJorsbez7ZqC/SlHp5Vu4fJ8lL/cQf2d9vh4Ub0o9FiZpI.l.aG'} )
+
+@euser = User.new(:email => 'e@gmail.com', :password => 'password', :password_confirmation => 'password', :first_name => 'First', :last_name => 'Last', :major => 'Engineering')
+@euser.save
+
+@buser = User.new(:email => 'b@gmail.com', :password => 'password', :password_confirmation => 'password', :first_name => 'First', :last_name => 'Last', :major => 'Business')
+@buser.save
