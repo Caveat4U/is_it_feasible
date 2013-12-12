c2d9bbf45b312f1176660ab731d77801552d79d3
Chris Sterling <sterlincexc@colorado.edu>
2013-12-11 23:52:28 -0800

    Changing URL - probably premature...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ IsItFeasible::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Change mailer for devise
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'secret-garden-9030.heroku.com' }
 
   # Code is not reloaded between requests
   config.cache_classes = true
@@ -67,4 +67,7 @@ IsItFeasible::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+config.active_record.mass_assignment_sanitizer = false
+
 end
